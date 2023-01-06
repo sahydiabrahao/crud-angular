2498bf17336fa5e9fb89422307ebcaafe9992716
sahydi abrahao <sahydi@protonmail.com>
2023-01-05 23:21:50 -0400

    1.1 Color OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud-angular';
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
+  OnInit(){
+  }
+
+  //Menu Lateral
+  onContactBook(){
+    this.router.navigate(['contact-book'], {relativeTo: this.route});
+  }
+
+  onAdd(){
+    this.router.navigate(['contact-book/add'], {relativeTo: this.route});
+  }
+
+  onReport(){
+    this.router.navigate(['contact-book/report'], {relativeTo: this.route});
+  }
+
+  onLogout(){
+    localStorage.clear();
+    this.router.navigate([''], {relativeTo: this.route});
+  }
+
 }
