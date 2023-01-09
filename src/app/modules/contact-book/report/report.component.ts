b208aee768c8e24da1b11c8e11073853f4a6d8d3
sahydi abrahao <sahydi@protonmail.com>
2023-01-09 13:26:43 -0400

    1.8 Report OK

--- src/app/modules/contact-book/report/report.component.ts
+++ src/app/modules/contact-book/report/report.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ReportModel } from 'src/app/model/ReportModel';
+import { ContactBookService } from 'src/app/service/contact-book.service';
 
 @Component({
   selector: 'app-report',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  user_id = parseInt(localStorage.getItem('user_id'));
+
+  showModal: boolean = false;
+  showDate: boolean = false;
+
+  report = new ReportModel();
+
+  constructor(
+    private router : Router,
+    private contactBookService : ContactBookService,
+  ) { }
 
   ngOnInit() {
+    if(localStorage.getItem('token') == null){
+      this.router.navigate(['']);
+    }
   }
+
+  onGenerate () {
+    this.showModal = !this.showModal;
+
+    this.contactBookService.contactReport();
+
+  }
+  onClose(){
+    this.showModal = !this.showModal;
+  }
+  onReport () {
+    this.showDate = false;
+  }
+
+  onReportDate () {
+    this.showDate = true;
+  }
+
 
 }
