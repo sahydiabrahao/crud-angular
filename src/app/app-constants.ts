b7171689c7d29248c3fd5da0d03b145cde0f95c0
sahydi abrahao <sahydi@protonmail.com>
2023-01-12 13:54:44 -0400

    2.0 OnRender

--- src/app/app-constants.ts
+++ src/app/app-constants.ts
@@ -1,6 +1,8 @@
 export class AppConstants {
 
-  public static get baseUrl(): string { return "http://localhost:8081/"};
+  public static get baseUrl(): string { return "https://crud-spring.onrender.com/"};
+
+  //public static get baseUrl(): string { return "http://localhost:8081/"};
 
   public static get baseContext(): string { return this.baseUrl + "sahydi"};
 
